fa0e1278444080fd3aa9d12c81739e312d57c6a2
aomotere <115338947+aomotere@users.noreply.github.com>
2022-11-29 15:26:42 -0800

    create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+print('simon')
+print ('Ivo')
+# echo simon
+print ('chidi')
+print ('grace')
+print ('glory')
+print ('LSS')
+# bugs remove
+print (" car services automated")
+print ("hello $user your need serving")
